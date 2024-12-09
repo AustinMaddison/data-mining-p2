d7c9cc3da2ae3e03930a5c4687f23f00396a52e7
shin <shin@shins-MacBook-Air.local>
2024-12-10 00:35:30 +0700

    logstash shall be fixed but kibana index is weird

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -2,9 +2,10 @@ from kafka import KafkaConsumer
 from elasticsearch import Elasticsearch
 import json
 
-consumer = KafkaConsumer('financial-data', bootstrap_servers='localhost:29092', value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+consumer = KafkaConsumer('financial-data', bootstrap_servers='localhost:29092', value_deserializer=lambda m: json.loads(m.decode('utf-8')), auto_offset_reset='earliest', group_id=None)
 es = Elasticsearch(['http://localhost:9200'])
 
 for message in consumer:
     data = message.value
-    es.index(index='financial-data', body=data)
+    print(data['Meta Data'])
+    es.index(index='financial-data', body=data['Meta Data'])
--- kafka/producer.py
+++ kafka/producer.py
@@ -1,9 +1,26 @@
+from time import sleep
+
 from kafka import KafkaProducer
 import json
 import requests
 
 import os
 from dotenv import load_dotenv
+from kafka.admin import KafkaAdminClient, NewTopic
+
+# Kafka admin setup
+admin = KafkaAdminClient(
+    bootstrap_servers='localhost:29092',
+)
+topics_list = admin.list_topics()
+# Admin logic
+
+# if "financial-data" not in topics_list:
+#   topics = [NewTopic(name="financial-data", num_partitions=2, replication_factor=1)]
+#   admin.create_topics(new_topics=topics, validate_only=False)
+#
+# else:
+#        print(topics_list)
 
 
 load_dotenv()
@@ -12,16 +29,18 @@ producer = KafkaProducer(bootstrap_servers='localhost:29092', value_serializer=l
 
 def fetch_financial_data(api_key, symbol):
 
-    url = f""
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=5min&apikey={api_key}'
     response = requests.get(url)
     data = response.json()
+    #print(data)
     return data
 
 def stream_data():
     api_key = os.getenv('API_KEY')
-    
-    data = fetch_financial_data()
+    data = fetch_financial_data(api_key, 'aapl')
+    #print(data)
     producer.send('financial-data', data)
+    producer.flush()
 
 if __name__ == "__main__":
     stream_data()
