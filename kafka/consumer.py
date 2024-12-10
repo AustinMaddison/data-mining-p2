a025a9e40bc796f882252999a9b6d7dc306d3847
Attaphat Chana <attaphart@gmail.com>
2024-12-11 00:08:49 +0700

    testing alpaca marketing api

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -17,6 +17,5 @@ end_offset = consumer.end_offsets([partition])
 consumer.seek(partition, list(end_offset.values())[0]-1)
 for message in consumer:
     data = message.value
-    print(data)
-    es.index(index='financial-data', body=data)
-
+    print(data['Meta Data'])
+    es.index(index='financial-data', body=data['Meta Data'])
