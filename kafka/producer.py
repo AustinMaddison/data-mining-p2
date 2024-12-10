5917f3d6ad904764266cae726186725052a63659
shin <shin@shins-MacBook-Air.local>
2024-12-10 23:29:41 +0700

    made alpaca trade data json and it now appears in apache thingy

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,5 +1,9 @@
+import pprint
 from time import sleep
 
+import pandas as pd
+from alpaca.data import Trade
+from alpaca_trade_api.entity import ISO8601YMD, _NanoTimestamped
 from kafka import KafkaProducer
 import json
 import requests
@@ -7,6 +11,9 @@ import requests
 import os
 from dotenv import load_dotenv
 from kafka import KafkaAdminClient
+from sqlalchemy.util import symbol
+import time
+
 # from kafka.admin import KafkaAdminClient
 
 # Kafka admin setup
@@ -36,11 +43,42 @@ def fetch_financial_data(api_key, symbol):
     return data
 
 def stream_data():
-    api_key = os.getenv('API_KEY')
-    data = fetch_financial_data(api_key, 'aapl')
-    producer.send('financial-data', data)
-    print(data)
+    f = open('symbols.json')
+    symbols = json.load(f)
+    for symbol in symbols:
+        api_key = os.getenv('API_KEY')
+        apiKey = '6E87VKYTFU8HOT6J'
+        apiKey2= 'X4FDF7HQHOIV0YF9'
+        data = fetch_financial_data(apiKey2, symbol["symbol"])
+        producer.send('financial-data', data)
+        producer.flush()
+
+
+from alpaca_trade_api.common import URL
+from alpaca_trade_api.stream import Stream
+
+
+async def trade_callback(t):
+    trade_data = t if isinstance(t, dict) else t.__dict__
+    print(trade_data)
+    producer.send('financial-data', trade_data)
     producer.flush()
 
+
+# async def quote_callback(q):
+#     return q
+
+
+
+# Initiate Class Instance
+stream = Stream("PKHL0MBLB5UWPWU4GJTP",
+                "Zms9rxshMqgtZeX6rlUHuusj7PGrtGlPhNR20lbP",
+                base_url=URL('wss://stream.data.alpaca.markets/v2/'),
+                data_feed='iex')  # <- replace to 'sip' if you have PRO subscription
+
+# subscribing to event
+stream.subscribe_trades(trade_callback, 'AAPL')
+#stream.subscribe_quotes(quote_callback, 'IBM')
+
 if __name__ == "__main__":
-    stream_data()
+    stream.run()
